a3e8b75863abe06484d511a25cd326875c90c765
ezequielaranda81 <ezequiel.aranda@gmail.com>
2020-05-30 16:54:43 -0300

    commit

--- backend/api/migrations/0008_auto_20200530_1634.py
+++ backend/api/migrations/0008_auto_20200530_1634.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0007_auto_20200528_0952'),
+        ('api', '0005_auto_20200528_0952'),
     ]
 
     operations = [
